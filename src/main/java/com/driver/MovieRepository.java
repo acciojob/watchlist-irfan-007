c01aab68e5eb0afdf10f950dba264bf1195323e3
irfan <85334710+irfan-007@users.noreply.github.com>
2024-04-29 13:49:38 +0530

    Update MovieRepository.java

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -19,28 +19,36 @@ public class MovieRepository {
 
     public void saveMovie(Movie movie){
         // your code here
+        movieMap.put(movie.getName(), movie);
     }
 
     public void saveDirector(Director director){
         // your code here
+        directorMap.put(director.getName(), director);
     }
 
     public void saveMovieDirectorPair(String movie, String director){
         if(movieMap.containsKey(movie) && directorMap.containsKey(director)){
             // your code here
+            List<String> tem=directorMovieMapping.getOrDefault(director,new ArrayList<>());
+            tem.add(movie);
+            directorMovieMapping.put(director,tem);
         }
     }
 
     public Movie findMovie(String movie){
         // your code here
+        return (movieMap.containsKey(movie))? movieMap.get(movie):null;
     }
 
     public Director findDirector(String director){
         // your code here
+        return (directorMap.containsKey(director))? directorMap.get(director):null;
     }
 
     public List<String> findMoviesFromDirector(String director){
         // your code here
+        return (directorMovieMapping.containsKey(director))? directorMovieMapping.get(director):null;
     }
 
     public List<String> findAllMovies(){
@@ -49,9 +57,13 @@ public class MovieRepository {
 
     public void deleteDirector(String director){
         // your code here
+        directorMap.remove(director);
+        directorMovieMapping.remove(director);
     }
 
     public void deleteAllDirector(){
         // your code here
+        directorMap.clear();
+        directorMovieMapping.clear();
     }
 }
